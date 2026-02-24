87cbdc6cdce5b2e0d7649158019483f858f641a6
mahabur-rahman <annur4395@gmail.com>
2026-02-24 22:15:02 +0600

    Fix desktop shortcuts navigation by reacting to hash/path changes

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react'
 import { DesktopNotesLayout } from './components/layout/DesktopNotesLayout'
 import { PrivacyPolicyPage } from './components/help/PrivacyPolicyPage'
 import { ShortcutsPage } from './components/help/ShortcutsPage'
 import { APP_TITLE, MENU_ITEMS } from './constants/ui'
 
-function App(): React.JSX.Element {
+function getActivePath(): string {
   const normalizedPath = window.location.pathname.replace(/\/+$/, '') || '/'
   const normalizedHashPath = window.location.hash.replace(/^#/, '').replace(/\/+$/, '') || '/'
-  const activePath = normalizedHashPath !== '/' ? normalizedHashPath : normalizedPath
+  return normalizedHashPath !== '/' ? normalizedHashPath : normalizedPath
+}
+
+function App(): React.JSX.Element {
+  const [activePath, setActivePath] = useState<string>(() => getActivePath())
+
+  useEffect(() => {
+    const handleLocationChange = (): void => {
+      setActivePath(getActivePath())
+    }
+
+    window.addEventListener('hashchange', handleLocationChange)
+    window.addEventListener('popstate', handleLocationChange)
+
+    return () => {
+      window.removeEventListener('hashchange', handleLocationChange)
+      window.removeEventListener('popstate', handleLocationChange)
+    }
+  }, [])
 
   if (activePath === '/keyboard-shortcuts' || activePath === '/shortcuts') {
     return <ShortcutsPage />
